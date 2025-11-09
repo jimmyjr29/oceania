14b84fae60acc6e7bbda50c15c1eeeacb60ce5c8
Jimsjr <fiverjim29@gmail.com>
2025-11-09 14:25:18 +0700

    fix eror scroll

--- components/ScrollVelocity.tsx
+++ components/ScrollVelocity.tsx
@@ -7,7 +7,7 @@ export const ScrollVelocity = () => {
   return (
     <ScrollVelocityContainer className="bg-orange-100/50 text-2xl text-oceania-navy md:text-4xl font-bold select-none">
       <ScrollVelocityRow baseVelocity={20} direction={1}>
-        <Image src="/logo.svg" alt="Scroll 1" width={50} height={50} className="mx-2 inline-block" />
+        <Image src="/logo.svg" alt="Logo Oceania Travel" width={50} height={50} className="mx-2 inline-block" />
         OCEANIA
       </ScrollVelocityRow>
     </ScrollVelocityContainer>
